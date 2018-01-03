1afdc6349098b0303c678a70a04d7ec58c00eafa
irwanputrasetia <irwanputrastya@gmail.com>
2018-01-03 19:09:36 +0700

    membuat variable k2 dengan array 2 dimensi

--- tgs_thnbaru.cpp
+++ tgs_thnbaru.cpp
@@ -15,6 +15,7 @@ int main(){
 	}while(audey1<5);cout<<endl;
 	int audey2=0;
 	do{
+		cout << a2c[audey2]<<" | ";
 		audey2++;
 	}while(audey2<5);cout<<endl;
 		int audey3=0;
@@ -22,6 +23,15 @@ int main(){
 		cout << a3c[audey3]<<" | ";
 		audey3++;
 	}while(audey3<5);cout<<endl;
-
+		
+	double mc1=max(a1c[0],max(a2c[0],a3c[0])),
+	mc2=max(a1c[1],max(a2c[1],a3c[1])),
+	mc3=max(a1c[2],max(a2c[2],a3c[2])),
+	mc4=max(a1c[3],max(a2c[3],a3c[3])),
+	mc5=max(a1c[4],max(a2c[4],a3c[4]));
+	
+	double Ra1c[5]={ (a1c[0]/mc1),(a1c[1]/mc2),(a1c[2]/mc3),(a1c[3]/mc4),(a1c[4]/mc5) };
+	double Ra2c[5]={ (a2c[0]/mc1),(a2c[1]/mc2),(a2c[2]/mc3),(a2c[3]/mc4),(a2c[4]/mc5) };
+	double Ra3c[5]={ (a3c[0]/mc1),(a3c[1]/mc2),(a3c[2]/mc3),(a3c[3]/mc4),(a3c[4]/mc5) };
 }
 
